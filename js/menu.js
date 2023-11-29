954acd5fc8b48172d53ac1ded35838c79675b535
walmirsanto <rosacardosowalmir@gmail.com>
2023-11-29 08:19:51 -0300

    teste do menu

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+// menu.js
+document.addEventListener("DOMContentLoaded", function () {
+    // Carregar o conteúdo do menu
+    var menuContainer = document.getElementById("menuContainer");
+
+    // Verificar se o contêiner do menu existe na página
+    if (menuContainer) {
+        // Carregar o conteúdo do menu.html no contêiner do menu
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "menu.html", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                menuContainer.innerHTML = xhr.responseText;
+
+                // Destacar o item do menu correspondente à página atual
+                highlightCurrentPage();
+            }
+        };
+        xhr.send();
+    }
+});
